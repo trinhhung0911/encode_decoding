7945d61df80a3ccca59cfb1c872062d6479a1d58
OOP <tomodachy2000@gmail.com>
2021-05-01 08:28:57 +0700

    up 1

---
+++ C code/firstPro.cpp
@@ -0,0 +1,15 @@
+//#include<bits/stdc++.h>
+//#include<stdio.h>
+#include<iostream>
+//#define for(i,a,b) for(int i=a;i<b;i++)
+//#ifndef __xxx__cpp__
+//#define __xxx__cpp__
+using namespace std;
+int main()
+{
+	system("color 2f");
+	cout<<"Xin chao!";
+	return 0;
+}
+//#endif
+
